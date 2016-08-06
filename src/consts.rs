6de1a36a6e55d6e487d13e50d812aa573b9c7c9e
Jesse J. Bethel <keisetsu@gmail.com>
2016-08-06 11:26:03 -0500

    (Mostly) finished rust tcod tutorial Added bluebox font Added background for main menu from tutorial Moved constants into separate file

---
+++ src/consts.rs
@@ -0,0 +1,44 @@
+extern crate tcod;
+
+use tcod::colors::{self, Color};
+use tcod::map::FovAlgorithm;
+
+pub const SCREEN_WIDTH: i32 = 80;
+pub const SCREEN_HEIGHT: i32 = 50;
+pub const LIMIT_FPS: i32 = 20;
+
+pub const BAR_WIDTH: i32 = 20;
+pub const PANEL_HEIGHT: i32 = 7;
+pub const PANEL_Y: i32 = SCREEN_HEIGHT - PANEL_HEIGHT;
+
+pub const MSG_X: i32 = BAR_WIDTH + 2;
+pub const MSG_WIDTH: i32 = SCREEN_WIDTH - BAR_WIDTH - 2;
+pub const MSG_HEIGHT: usize = PANEL_HEIGHT as usize - 1;
+
+pub const MAP_WIDTH: i32 = 80;
+pub const MAP_HEIGHT: i32 = 43;
+
+pub const MAX_INVENTORY_ITEMS: i32 = 26;
+pub const INVENTORY_WIDTH: i32 = 50;
+
+pub const COLOR_DARK_WALL: Color = colors::DARKEST_GREY;
+pub const COLOR_LIGHT_WALL: Color = colors::DARKER_GREY;
+pub const COLOR_DARK_GROUND: Color = colors::DARK_GREY;
+pub const COLOR_LIGHT_GROUND: Color = colors::LIGHT_GREY;
+
+// pub const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+// pub const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b:50 };
+// pub const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+// pub const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50 };
+
+pub const ROOM_MAX_SIZE: i32 = 10;
+pub const ROOM_MIN_SIZE: i32 = 6;
+pub const MAX_ROOMS: i32 = 30;
+pub const MAX_ROOM_MONSTERS: i32 = 3;
+pub const MAX_ROOM_ITEMS:i32 = 4;
+
+pub const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
+pub const FOV_LIGHT_WALLS: bool = true;
+pub const TORCH_RADIUS: i32 = 10;
+
+pub const PLAYER: usize = 0;
